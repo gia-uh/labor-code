37fc29e176078c5f2863142c840371dc96643604
dvaldes8128 <d.valdes8128@gmail>
2025-09-18 12:07:39 -0400

    Add pagination to search results, refactor client initialization,

--- main.py
+++ main.py
@@ -120,7 +120,14 @@ if not st.session_state.logged_in:
     pg = st.navigation([login_page])
 else:
     pg = st.navigation(
-        [intro_page,ideas_page,project_page,chat_page, search_page,logout_page]
+        [
+            intro_page,
+            ideas_page,
+            project_page,
+            chat_page,
+            search_page,
+            logout_page,
+            ]
     )
 
 pg.run()
